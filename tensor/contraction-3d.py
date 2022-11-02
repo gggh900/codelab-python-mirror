c46e81fad86476bf7788a394f73640878cafb334
Your Name <you@example.com>
2022-11-02 22:44:59 +0000

    contraction-3d.py/contraction-4d.py: improvement in coding result still does not match"

--- tensor/contraction-3d.py
+++ tensor/contraction-3d.py
@@ -1,21 +1,67 @@
+
 import numpy as np
+
+CONFIG_UNIFORM_DIM=0
+ENABLE_CONTRACTION=1
 d=10
 A=np.random.rand(d+0,d+1,d+2)
 B=np.random.rand(d+0,d+2,d+1)
 
-'''
-Ap=np.transpose(A, (0,2,1,3))
-Bp=np.transpose(B, (0,3,1,2))
-App=np.reshape(Ap, ((d+0) * (d+1), (d+2) * (d+3)))
-Bpp=np.reshape(Bp, ((d+3) * (d+2), (d+1) * (d+0)))
+if CONFIG_UNIFORM_DIM:
+    A=np.random.rand(d,d,d)
+    B=np.random.rand(d,d,d)
 
-print("A, Ap, App: ", A.shape, Ap.shape, App.shape)
-print("B, Bp, Bpp: ", B.shape, Bp.shape, Bpp.shape)
-'''
+if ENABLE_CONTRACTION:
+    Ap=np.transpose(A, (0,2,1))
+    Bp=np.transpose(B, (0,2,1))
 
-print("A: ", A.shape)
-print("B: ", B.shape)
+    #Ap=np.transpose(A, (0,2,1,3))
+    #Bp=np.transpose(B, (0,3,1,2))
+
+    if not CONFIG_UNIFORM_DIM:
+        App=np.reshape(Ap, ((d+0), (d+1) * (d+2)))
+        Bpp=np.reshape(Bp, ((d+0), (d+2) * (d+1)))
+    else:
+        App=np.reshape(Ap, (d,d*d))
+        Bpp=np.reshape(Bp, (d,d*d))
+
+
+if ENABLE_CONTRACTION:
+    print("A, Ap, App: ", A.shape, Ap.shape, App.shape)
+    print("B, Bp, Bpp: ", B.shape, Bp.shape, Bpp.shape) 
+else:
+    print("A: ", A.shape)
+    print("B: ", B.shape) 
+
+
 C=np.matmul(A, B)
 print("C: ", C.shape)
-#Cpp=np.matmul(App, Bpp)
-#print("Cpp: ", Cpp.shape)
+
+if ENABLE_CONTRACTION:
+    Cpp=np.matmul(App, Bpp)
+
+    if not CONFIG_UNIFORM_DIM:
+        C1=np.reshape(Cpp, (d+0, d+1, d+1))
+    else:
+        C1=np.reshape(Cpp, (d,d,d))
+
+    print("C1: ", C1.shape)
+
+    if np.array_equal(C1, C):
+        print("arrays match.")
+    else:
+        print("!arrays do not match")
+        counter = 0
+        nonMatch=0
+
+        for i in range(0,d):
+            for j in range(0,d):
+                for k in range(0,d):
+                        if C[i,j,k] != C1[i,j,k]:
+                            nonMatch+=1
+                            if counter < 10:
+                                print("non matching element: [", i,j,k,"]", C[i,j,k], C1[i,j,k])
+                            counter+=1
+        print("Non-matching elements: ", nonMatch)
+
+
