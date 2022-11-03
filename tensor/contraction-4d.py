33e4ddb3d0f2c37efed943d32472f390d3c91ec9
Your Name <you@example.com>
2022-11-03 04:08:10 +0000

    contraction-4d.py: data type support added, specifically int and potentially more

--- tensor/contraction-4d.py
+++ tensor/contraction-4d.py
@@ -1,13 +1,34 @@
 import numpy as np
+
 ENABLE_CONTRACTION=1
 CONFIG_UNIFORM_DIM=1
+CONFIG_PRINT_TENSORS=1
+
+CONFIG_DATA_TYPE_INT=0
+CONFIG_DATA_TYPE_FP32=1
+CONFIG_DATA_TYPE=CONFIG_DATA_TYPE_INT
 d=2
-A=np.random.rand(d+0,d+1,d+2,d+3)
-B=np.random.rand(d+0,d+1,d+3,d+2)
+
+if CONFIG_DATA_TYPE==CONFIG_DATA_TYPE_FP32:
+    A=np.random.rand(d+0,d+1,d+2,d+3)
+    B=np.random.rand(d+0,d+1,d+3,d+2)
+elif CONFIG_DATA_TYPE==CONFIG_DATA_TYPE_INT:
+    A=np.random.randint(0, high=100, size=(d+0,d+1,d+2,d+3))
+    B=np.random.randint(0, high=100, size=(d+0,d+1,d+3,d+2))
+else:
+    print("Data type not specified....")
+    exit(1)
 
 if CONFIG_UNIFORM_DIM:
-    A=np.random.rand(d,d,d,d)
-    B=np.random.rand(d,d,d,d)
+    if CONFIG_DATA_TYPE==CONFIG_DATA_TYPE_FP32:
+        A=np.random.rand(d,d,d,d)
+        B=np.random.rand(d,d,d,d)
+    elif CONFIG_DATA_TYPE==CONFIG_DATA_TYPE_INT:
+        A=np.random.randint(0, high=100, size=(d,d,d,d))
+        B=np.random.randint(0, high=100, size=(d,d,d,d))
+    else:
+        print("Data type not specified....")
+        exit(1)
 
 if ENABLE_CONTRACTION:
     Ap=np.transpose(A, (0,2,1,3))
@@ -21,13 +42,25 @@ if ENABLE_CONTRACTION:
         Bpp=np.reshape(Bp, (d*d,d*d))
 
 if ENABLE_CONTRACTION:
-    print("A, Ap, App: ", A.shape, Ap.shape, App.shape)
-    print("B, Bp, Bpp: ", B.shape, Bp.shape, Bpp.shape)
+    print("A, Ap, App (shape): ", A.shape, Ap.shape, App.shape)
+    print("B, Bp, Bpp (shape): ", B.shape, Bp.shape, Bpp.shape)
+
+    if CONFIG_PRINT_TENSORS:
+        print("A, Ap, App: \n", A, "\n", Ap, "\n", App)
+        print("B, Bp, Bpp: \n", B, "\n", Bp, "\n", Bpp)
+        
 else:
     print("A, B: ", A.shape, B.shape)
 
+    if CONFIG_PRINT_TENSORS:
+        print("A: \n", A)
+        print("B: \n", B)
+
 C=np.matmul(A, B)
-print("C: ", C.shape)
+print("C.shape: ", C.shape)
+
+if CONFIG_PRINT_TENSORS:
+    print("C: \n", C)
 
 if ENABLE_CONTRACTION:
     Cpp=np.matmul(App, Bpp)
@@ -38,6 +71,8 @@ if ENABLE_CONTRACTION:
         C1=np.reshape(Cpp, (d,d,d,d))
 
     print("C1: ", C1.shape)
+    if CONFIG_PRINT_TENSORS:
+        print("C1: ", C1)
 
     if np.array_equal(C1, C):
         print("arrays match.")
