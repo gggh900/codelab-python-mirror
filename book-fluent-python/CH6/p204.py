f1914dfba79f9a0cf5413a9ae901ef3b36552558
Your Name <you@example.com>
2022-12-18 23:20:44 -0800

    p204.py: added

--- book-fluent-python/CH6/p204.py
+++ book-fluent-python/CH6/p204.py
@@ -1,7 +1,14 @@
 charles={'name': 'Charles L.Dodgson', 'born': 1832}
 lewis=charles
-lewis is charles
+print(lewis is charles)
 print(id(charles), id(lewis))
 lewis['balance']=950
 print(charles)
 print(lewis)
+
+charles={'name': 'Charles L.Dodgson', 'born': 1832}
+alex={'name': 'Charles L.Dodgson', 'born': 1832}
+print("alex==charles:", alex==charles)
+print(alex is not charles)
+print(alex is charles)
+print(id(alex))
