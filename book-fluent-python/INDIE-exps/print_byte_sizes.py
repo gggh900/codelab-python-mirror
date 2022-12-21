098141e1b17db4ebe17f8c8ce8e2dcd90e347084
Your Name <you@example.com>
2022-12-21 09:54:16 -0800

    p232.py: added

--- book-fluent-python/INDIE-exps/print_byte_sizes.py
+++ book-fluent-python/INDIE-exps/print_byte_sizes.py
@@ -2,10 +2,14 @@ import sys
 import array as arr
 
 a=2
-b=[1,2]
-b1=arr.array('i',[1,2])
+b=list(range(0,100))
+b1=arr.array('I', range(0,100))
 
 c={1:"a", 2: "b"}
 for i in [a,b,b1,c]:
     print("printing the byte size of ", type(i))
+    try:
+        print("len: ", len(i))
+    except Exception as msg:
+        print("len: None")
     print(sys.getsizeof(i))
