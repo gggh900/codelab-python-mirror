a4c6bc9bc1264a93b91fd8fdd7e2783b31f8ad38
Your Name <you@example.com>
2022-12-21 10:00:44 -0800

    p234.py: added

---
+++ book-fluent-python/CH7/p234.py
@@ -0,0 +1,9 @@
+fruits=['grape','raspberry','apple','banana']
+print(fruits)
+def reverse(word):
+    return word[::-1]
+
+print(reverse('testing'))
+
+print(sorted(fruits, key=reverse))
+print(fruits)
