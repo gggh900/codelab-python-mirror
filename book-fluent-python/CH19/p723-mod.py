1671d8a7b97cb4042d2c6b9d78ace1885e3300a3
Your Name <you@example.com>
2022-12-29 10:37:20 -0800

    p723-mod.py: code complete, still not running

---
+++ book-fluent-python/CH19/p723-mod.py
@@ -0,0 +1,56 @@
+import sys
+import time
+import random
+from time import perf_counter
+from typing import NamedTuple
+from multiprocessing import Process, SimpleQueue, cpu_count
+from multiprocessing import queues
+NUMBERS=range(20, 40)
+
+class RandomSleepResult(NamedTuple):
+    elapsed: float
+
+JobQueue=queues.SimpleQueue[int]
+ResultQueue=queues.SimpleQueue[RandomSleepResult]
+
+def printFunctionEntry(func):
+    def inner(*args):
+        name=func.__name__
+        print(f'[{name} entered...')
+        result=func(*args)
+        return result
+    return inner
+
+#@printFunctionEntry
+def randomSleep(n:int)->RandomSleepResult:
+    #t0=perf_counter()
+    #res=is_prime(n)
+    #return RandomSleepResult(n, res, perf_counter()-t0)
+    sleepDuration=random.randint(20, 40)
+    time.sleep(sleepDuration)
+    return RandomSleepResult(sleepDuration)
+
+#@printFunctionEntry
+def worker(jobs: JobQueue)->None:
+    while n:=jobs.get():
+        result.put(randomSleep(n))    
+    #results.put(RandomSleepResult(0, False, 0.0))
+        
+@printFunctionEntry
+def start_jobs(procs: int, jobs: JobQueue)->None:
+    for n in NUMBERS:
+        jobs.put(n)
+    for _ in range(procs):
+        proc=Process(target=worker, args=(jobs))
+        proc.start()
+        jobs.put()
+
+if len(sys.argv) < 2:
+    procs=cpu_count()
+else:
+    procs=int(sys.argv[1])
+
+jobs: JobQueue=SimpleQueue()
+#results: ResultsQueue=SimpleQueue()
+start_jobs(procs, jobs)
+    
