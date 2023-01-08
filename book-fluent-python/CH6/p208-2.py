ed559e9faf2e03c15ad358abd8eccd8f1a13f753
Your Name <you@example.com>
2023-01-08 02:07:55 +0000

    p208-2.py: enhancement

--- book-fluent-python/CH6/p208-2.py
+++ book-fluent-python/CH6/p208-2.py
@@ -1,3 +1,8 @@
+'''
+l2 creates new separate entity not alias.
+Therefore l2==l1 compares value which results in true
+but l2 is l1 compares the object not just value which is different.
+'''
 l1=[3,[55,44], (7,8,9)]
 l2=list(l1)
 
