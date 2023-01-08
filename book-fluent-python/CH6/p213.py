620e446dcf54585af35ee6bc698395c8e5f7fb5d
Your Name <you@example.com>
2023-01-07 21:29:11 -0800

    p213.py: enhanced

--- book-fluent-python/CH6/p213.py
+++ book-fluent-python/CH6/p213.py
@@ -1,16 +1,27 @@
+''' 
+Example of function changing the mutable vs. immutable object 
+on return
+'''
+
 def f(a,b):
     a+=b
     return a
+
+# x is changed. 
 
 x=1
 y=2
 print(f(x,y))
 print(x,y)
 
+# a is changed.
+
 a=[1,2]
 b=[3,4]
 print(f(a,b))
 print(a,b)
+
+# t is not changed. 
 
 t=(10,20)
 u=(30,40)
