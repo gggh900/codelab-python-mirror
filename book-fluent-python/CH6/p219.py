1076e04174320baed95b53a002cb0f07ee094879
Your Name <you@example.com>
2023-01-07 21:30:56 -0800

    p219.py enhancement

--- book-fluent-python/CH6/p219.py
+++ book-fluent-python/CH6/p219.py
@@ -1,6 +1,16 @@
+
 a=[1,2]
+
+# b is a 2nd reference to a.
+
 b=a
+
+# object still exists since b is pointer but a is no longer pointing to [1,2] 
+# so print should be ok.
+
 del a
 print(b)
+
+# b is reasiigned to object [1,2] should not exist. and b has no value.
 b=[3]
 print(b)
