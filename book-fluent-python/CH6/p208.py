49a9201d4c244db6828f2b302ac73d216149a8bb
Your Name <you@example.com>
2023-01-08 02:13:41 +0000

    p208.py: enhancement

--- book-fluent-python/CH6/p208.py
+++ book-fluent-python/CH6/p208.py
@@ -1,9 +1,26 @@
+# by changing t1[-1], t1[-1] itself being mutable, its id remain unchanged and remained same object.
+# so does t1 which was not assigned, only its member (list) has changed to remained same object.
+
+# t1 is mutable but t1[-1] is not mutable.
+
 t1=(1,2,[30,40])
+
+#  build t2 whose items are equal to those of t1.
+
 t2=(1,2,[30,40])
+
+# modify t1[-1] list in place.
+
 print("t1==t2:", t1==t2)
+
+# identity of t1[-1] has not changed, only its value.
+
 print("id(t1[-1]:", id(t1[-1]))
 t1[-1].append(99)
 print(t1)
 print("id(t1[-1]:", id(t1[-1]))
+
+# t1 and t2 is not different.
+
 print("t1==t2:", t1==t2)
 
