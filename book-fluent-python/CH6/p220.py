52004142616ec094af34069b15b8cd1b5ea32553
Your Name <you@example.com>
2023-01-07 21:34:28 -0800

    p220.py: enhancement

--- book-fluent-python/CH6/p220.py
+++ book-fluent-python/CH6/p220.py
@@ -2,12 +2,20 @@ import weakref
 s1={1,2,3}
 s2=s1
 
+# weak references
+
 def bye():
     print("...like tears in the rain.")
 
 ender=weakref.finalize(s1, bye)
 print(ender.alive)
+
+#s1 is pointing away from {1,2,3} now.
+
 del s1
 print(ender.alive)
+
+#s2 is pointing away from {1,2,3} now. Nothing should be pointing.
+
 s2='spam'
 print(ender.alive)
